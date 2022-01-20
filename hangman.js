cf8abfb717183b7666bf399c002a65e93f89626c
ikromshi <numanovikrom454@gmail.com>
2022-01-19 22:03:10 -0500

    setting the guessing feature and event listener (so user can geuss from the website

---
+++ hangman.js
@@ -0,0 +1,39 @@
+// Sets up the main part of the game (parameters --> Original word, Num of guesses)
+const Hangman = function(word, remainingGuesses) {
+    this.word = word.toLowerCase().split("")
+    this.remainingGuesses = remainingGuesses
+    
+    this.guessedLetters = []
+}
+
+// Decided if the guess is correct or not (add the letter if yes, asterisk otherwise)
+Hangman.prototype.puzzle = function() {
+    this.guessMatch = ""
+    this.word.forEach((letter, pos) => {
+        if (this.guessedLetters.includes(letter) || letter === " ") {
+            this.guessMatch += letter
+        } else {
+            this.guessMatch += "*"
+        }})
+    return this.guessMatch
+}
+
+// Enables user to make a guess (if correct fine, if not chances-1)
+Hangman.prototype.makeGuess = function (guess) {
+    this.guess = guess.toLowerCase()
+    if (!this.guessedLetters.includes(guess)) {
+        this.guessedLetters.push(guess)
+        this.puzzle()} 
+    if (!this.word.includes(guess)) {this.remainingGuesses -= 1}
+}
+
+const inst1 = new Hangman("Cat", 2)
+
+
+// Sets up the event listener so user can guess from the webpage
+window.addEventListener("keypress", function(e) {
+    const key = String.fromCharCode(e.charCode)
+    inst1.makeGuess(key)
+    console.log(inst1.guessMatch)
+    console.log(inst1.remainingGuesses)
+})
